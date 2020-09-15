eae9bcb147d1baebd23ac6bc77124bac425e0ea5
Tiborcz József <jozsef.tiborcz@gmail.com>
2020-09-15 06:45:10 +0200

    dummy commit

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index(String data) {
-		return "Greetings from Spring Boot!" + data;
+		return "Greetings from Spring Boot!" + data; 
 	}
 
 }
